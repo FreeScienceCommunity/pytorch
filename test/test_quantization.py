fc62d19debdf5d697e01c354994f8f61fcd67b2c
Jerry Zhang <jerryzh168@gmail.com>
2020-06-19 14:02:10 -0700

    [quant][graphmode] Rename graph mode quantization API from `*_script` to `*_jit`
    Summary:
    based on discussions in https://github.com/pytorch/pytorch/pull/40198
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: fa79a43c962242a06a7aef44c2c35c66ed7ba871
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40212

--- test/test_quantization.py
+++ test/test_quantization.py
@@ -50,12 +50,12 @@ from quantization.test_quantize import TestFunctionalModule  # noqa: F401
 from quantization.test_quantize import TestFusion  # noqa: F401
 from quantization.test_quantize import TestModelNumerics  # noqa: F401
 # 2. Graph mode quantization
-from quantization.test_quantize_script import TestQuantizeScriptJitPasses  # noqa: F401
-from quantization.test_quantize_script import TestQuantizeScriptPTSQOps  # noqa: F401
-from quantization.test_quantize_script import TestQuantizeDynamicScriptJitPasses  # noqa: F401
-from quantization.test_quantize_script import TestQuantizeScriptPTDQOps  # noqa: F401
-from quantization.test_quantize_script import TestQuantizeScript  # noqa: F401
-from quantization.test_quantize_script import TestQuantizeScriptJit  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeJitJitPasses  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeJitPTSQOps  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeDynamicJitPasses  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeJitPTDQOps  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeJit  # noqa: F401
+from quantization.test_quantize_jit import TestQuantizeJitJit  # noqa: F401
 
 # Tooling: numric_suite
 from quantization.test_numeric_suite import TestEagerModeNumericSuite  # noqa: F401
